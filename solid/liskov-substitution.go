07ff032aad4ea4806b77f1e007d20204ee1edea5
Last-dreamer <asim0000.ak@gmail.com>
2024-02-22 19:29:47 +0400

    add liskov substitute principle

---
+++ solid/liskov-substitution.go
@@ -0,0 +1,88 @@
+package solid
+
+import "fmt"
+
+// Liskov Substitutions Principle
+// named after barbara liskov principle
+// :=  Suppose we have an api that take base class & work correctly it should also worked in derived class
+
+type Sized interface {
+	GetWidth() int
+	SetWidth(width int)
+	GetHeight() int
+	SetHeight(height int)
+}
+
+type Rectangle struct {
+	width, height int
+}
+
+func (r *Rectangle) GetWidth() int {
+	return r.width
+}
+
+func (r *Rectangle) SetWidth(width int) {
+	r.width = width
+}
+
+func (r *Rectangle) GetHeight() int {
+	return r.height
+}
+
+func (r *Rectangle) SetHeight(height int) {
+	r.height = height
+}
+
+func UseIt(sized Sized) {
+	width := sized.GetWidth()
+	sized.SetHeight(10)
+	expectedArea := 10 * width
+	actualArea := sized.GetHeight() * sized.GetWidth()
+
+	fmt.Println("Expected area: ", expectedArea, "\n ", ", but got: ", actualArea)
+
+}
+
+type Square struct {
+	Rectangle
+}
+
+func NewSquare(size int) *Square {
+	sq := &Square{}
+	sq.width = size
+	sq.height = size
+	return sq
+}
+
+func (s *Square) SetWidth(width int) {
+	s.width = width
+	s.height = width
+}
+
+func (s *Square) SetHeight(height int) {
+	s.height = height
+	s.width = height
+}
+
+// frist approach
+
+type Square2 struct {
+	size int // width, height
+}
+
+func (s *Square2) Rectangle() *Rectangle {
+	return &Rectangle{
+		width:  s.size,
+		height: s.size,
+	}
+}
+
+func (s *Square2) SetWidth(width int) {
+	s.size = width
+	s.size = width
+}
+
+func (s *Square2) SetHeight(height int) {
+	s.size = height
+	s.size = height
+}
---
+++ solid/main_func_call.go
@@ -0,0 +1,66 @@
+package solid
+
+func MainFunctionCall() {
+
+	//! single responsibility principle
+	// j := Journal{}
+	// j.AddEntry("i wannan to go ")
+	// j.AddEntry("i wannan to go to the mall")
+	// fmt.Println(j.String())
+	//
+	// p := Persistance{LineSeparator: "\n"}
+	// p.SaveToFile(&j, "testing.txt")
+
+	//! open-closed principle
+	// apple := Product{
+	// 	Name:  "Apple",
+	// 	Color: green,
+	// 	Size:  small,
+	// }
+	// house := Product{
+	// 	Name:  "House",
+	// 	Color: blue,
+	// 	Size:  large,
+	// }
+	// tree := Product{
+	// 	Name:  "Tree",
+	// 	Color: green,
+	// 	Size:  large,
+	// }
+
+	// products := []Product{apple, house, tree}
+	// fmt.Printf("Green Old Products :\n")
+
+	// f := Filter{}
+	// for _, v := range f.FilterByColor(products, green) {
+	// 	fmt.Printf(" - %s is green\n", v.Name)
+	// }
+
+	// fmt.Printf("Green Product (New): \n")
+	// greenSpec := ColorSpicification{green}
+	// bf := BetterFilter{}
+	// for _, v := range bf.Filter(products, &greenSpec) {
+	// 	fmt.Printf(" - %s is green\n", v.Name)
+	// }
+
+	// fmt.Printf("Large Green Product: \n")
+	// largeSpec := SizeSpicification{large}
+	// lgSpec := AndSpecification{&largeSpec, &greenSpec}
+	// for _, v := range bf.Filter(products, &lgSpec) {
+	// 	fmt.Printf(" - %s is green\n", v.Name)
+	// }
+
+	//! liskov substitution principle
+
+	rc := &Rectangle{2, 3}
+	UseIt(rc) // 20, 20
+
+	// beaking the liskov substitution principle
+	sq := NewSquare(5)
+	UseIt(sq) // 20, 50
+
+	// frist solution
+	sq2 := &Square2{5}
+	UseIt(sq2.Rectangle()) // 50, 50
+
+}
